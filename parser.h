6464d0a53c5d2859b0b68c81a909744ff1c5445e
Random Internet Person <112799108+YardenDrori@users.noreply.github.com>
2025-02-28 12:00:59 +0200

    added a few grammer Symbols to parser.h

--- parser.h
+++ parser.h
@@ -3,8 +3,26 @@
 #include <stdlib.h>
 
 typedef enum {
-    EXAMPLE_SYMBOL
+    Expr,
+    Term,
+    Factor
 } grammarSymbol;
+
+/* FOR NOW ONLY THESE WILL BE USED
+Expr -> Expr + Term
+Expr -> Expr - Term
+Expr -> Term
+Term -> Term * Factor
+Term -> Term / Factor
+Term -> Factor
+Factor -> ( Expr )
+Factor -> id
+Factor -> intlit
+Factor -> id inc
+Factor -> id dec
+Factor -> dec id
+Factor -> inc id
+*/
 
 
 #endif
